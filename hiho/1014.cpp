bb4fdcf65fa7caaa4a7bd20f936d7ea8440749bf
hongchao <teddy.hongchao@gmail.com>
2016-05-05 13:42:50 +0800

    1014

--- hiho/1014.cpp
+++ hiho/1014.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+#define MAX_NUM 26
+
+enum NODE_TYPE{ //"COMPLETED" means a string is generated so far.
+  COMPLETED,
+  UNCOMPLETED
+};
+
+struct Node {
+  enum NODE_TYPE type;
+  char ch;
+  int count;
+  struct Node* child[MAX_NUM]; //26-tree->a, b ,c, .....z
+};
+
 int main()
 {
     struct
