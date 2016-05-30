808f8135f8e8b043727c3e633b3db93441c2a3d5
hongchao <teddy.hongchao@gmail.com>
2016-05-31 00:02:54 +0800

    finish leetcode/1

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -1,7 +1,26 @@
-##include <iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(7);
+    nums.push_back(11);
+    nums.push_back(15);
+    int target = 9;
+    
     vector<int> res;
+    int i, j;
+    for (i = 0; i < nums.size()-1; i++){
+        for (j = i + 1; j < nums.size(); j++){
+            if (nums[i] + nums[j] == target) {
+                res.push_back(i);
+                res.push_back(j);
+            }
+        }
+    }
+    cout<<res[0]<<"  "<<res[1]<<endl;
+    return 0;
 }
